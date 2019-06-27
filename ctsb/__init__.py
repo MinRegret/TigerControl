89536a319718710144f6200abb7109a74576937e
John Hallman <johnolof@gmail.com>
2019-06-26 22:18:24 -0400

    fixed LSTM with jax.numpy implementation

--- ctsb/__init__.py
+++ ctsb/__init__.py
@@ -9,13 +9,13 @@ from ctsb import error
 from ctsb.problems import Problem, problem, problem_registry, problem_spec
 from ctsb.models import Model, model, model_registry, model_spec
 from ctsb.help import help
-from ctsb.utils import tests
+from ctsb.utils import tests, set_key
 
 # initialize global random key by seeding the jax random number generator
 # note: numpy is necessary because jax RNG is deterministic
 import jax.random as random
 GLOBAL_RANDOM_KEY = random.PRNGKey(0)
-ctsb.utils.set_key()
+set_key()
 
 
-__all__ = ["Problem", "problem", "Model", "model", "help"]
+__all__ = ["Problem", "problem", "Model", "model", "help", "set_key"]
