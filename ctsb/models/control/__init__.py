d20f583b00262c3275369adf7c0c03f0d568663f
Paula Gradu <paulag@nat-tvwna-inside-visitornet101-10-25-33-89.princeton.org>
2019-07-07 23:21:09 -0400

    added shooting method

--- ctsb/models/control/__init__.py
+++ ctsb/models/control/__init__.py
@@ -2,3 +2,4 @@
 
 from ctsb.models.control.control_model import ControlModel
 from ctsb.models.control.kalman_filter import KalmanFilter
+from ctsb.models.control.shooting_method import ShootingMethod
