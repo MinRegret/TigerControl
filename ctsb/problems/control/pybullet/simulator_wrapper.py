b45adcf1fa8cdc19902afee493d879da3442d70f
AlexanderJYu <vengencemage@gmail.com>
2019-07-10 17:58:50 -0400

    tested simulator wrapper by stepping through problem, spawning off a simulation, and resuming the problemwhere it left off

--- ctsb/problems/control/pybullet/simulator_wrapper.py
+++ ctsb/problems/control/pybullet/simulator_wrapper.py
@@ -1,9 +1,13 @@
 
 from ctsb.problems.control.pybullet.pybullet_problem import PyBulletProblem
 
-class simulator_wrapper(PyBulletProblem):
-	def __init__(self, state):
-		self.state = state
+class SimulatorWrapper(PyBulletProblem):
+	def __init__(self, problem, state_id):
+		self.state_id = state_id
+		self.problem = problem
 
-	def set_state(state):
-		self.state = state
+	def set_state(self, state_id):
+		self.state_id = state_id
+
+	def step(self, action):
+		return self.problem.step(action)
