0ae76a9cf512f3d37887d6141033077ff8dc27bf
AlexanderJYu <vengencemage@gmail.com>
2019-07-11 14:41:46 -0400

    changed pybullet_problem.py, simulator_wrapper.py, cartpole.py and test_cartpole.py

--- ctsb/problems/control/pybullet/__init__.py
+++ ctsb/problems/control/pybullet/__init__.py
@@ -1,5 +1,4 @@
 # problems/control/pybullet problem init file
-
-from ctsb.problems.control.pybullet.kuka_diverse_object import KukaDiverseObject
 from ctsb.problems.control.pybullet.pybullet_problem import PyBulletProblem
+from ctsb.problems.control.pybullet.kuka_diverse_object import KukaDiverseObject
 from ctsb.problems.control.pybullet.cartpole import CartPole
--- ctsb/problems/control/pybullet/pybullet_problem.py
+++ ctsb/problems/control/pybullet/pybullet_problem.py
@@ -4,34 +4,31 @@
 from ctsb import error
 from ctsb.problems import Problem
 import pybullet as p
+from ctsb.problems.control.pybullet.simulator_wrapper import SimulatorWrapper
 
 # class for online control tests
 class PyBulletProblem(Problem):
     
-    # save sim state to disk
-    def saveToDisk(self, filename):
-        p.saveBullet(filename)
+    def __init__(self):
+        self.initialized = False
 
-    # load sim state from disk
-    def loadFromDisk(self, name):
-        p.restoreState(fileName = name)
-        self.updateState()
+    def initiliaze(self):
+        self.initialized = True
+        self.sim = None
 
-    # save state to memory
-    # keep track of state id
-    def saveToMemory(self):
-        stateID = p.saveState()
-        return stateID
+    def getState(self):
+        return self.sim.getState()
 
-    # load state from memory
-    def loadFromMemory(self, ID):
-        p.restoreState(stateId = ID)
-        # self.updateState()
-        
+    def loadState(self, id):
+        p.restoreState(stateId=id)
 
     # disconnect from physics server, end simulation
     def disconnect(self):
         p.disconnect()
+    
+    def get_simulator(self):
+        return self.sim
+
 
 
 
