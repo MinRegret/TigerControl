036cb62720e814f6a5249740991dca3c7b6be230
Paula Gradu <paulag@Paulas-MacBook-Pro.local>
2019-07-16 11:17:07 -0400

    changed ctrl_indices

--- ctsb/models/control/__init__.py
+++ ctsb/models/control/__init__.py
@@ -2,7 +2,6 @@
 
 from ctsb.models.control.control_model import ControlModel
 from ctsb.models.control.kalman_filter import KalmanFilter
-from ctsb.models.control.td3 import TD3
 from ctsb.models.control.ode_shooting_method import ODEShootingMethod
 from ctsb.models.control.lqr import LQR
 from ctsb.models.control.mppi import MPPI
