ad7074499690dad74c93eb6b240d48633b865862
John Hallman <johnolof@gmail.com>
2019-07-19 15:37:12 -0400

    fixed bugs

--- ctsb/models/optimizers/tests/run_all_tests.py
+++ ctsb/models/optimizers/tests/run_all_tests.py
@@ -5,8 +5,8 @@ from ctsb.models.optimizers.tests.test_sgd import test_sgd
 # run all optimizers tests
 def run_all_tests(steps=1000, show=False):
     print("\nrunning all optimizers tests...\n")
-    test_sgd(time=1)
-    test_adagrad(time=1)
+    test_sgd(time=1, show=show)
+    test_adagrad(time=1, show=show)
     print("\nall optimizers tests passed\n")
 
 
--- ctsb/models/optimizers/tests/test_adagrad.py
+++ ctsb/models/optimizers/tests/test_adagrad.py
@@ -4,16 +4,18 @@ from ctsb.models.optimizers.Adagrad import Adagrad
 from ctsb.experiments import Experiment
 
 
-def test_adagrad(time=1):
-	problem = ctsb.problem('ARMA-v0')
-	problem.initialize(p=2,q=0)
-	exp = Experiment()
-	# MSE = lambda y_true, y_pred: (y_true - y_pred)**2
-	problem_to_params = {'ARMA-v0' : {'p':2, 'q':0}} 
-	model_to_params = {'ArmaAdaGrad': {'p':3, 'optimizer': Adagrad, 'loss': mse}}
-	exp.initialize(mse, problem_to_params, model_to_params)
-	exp.run_all_experiments(time_steps=3000)
-	exp.plot_all_problem_results(time=time)
+def test_adagrad(time=1, show=False):
+    problem = ctsb.problem('ARMA-v0')
+    problem.initialize(p=2,q=0)
+    exp = Experiment()
+    # MSE = lambda y_true, y_pred: (y_true - y_pred)**2
+    problem_to_params = {'ARMA-v0' : {'p':2, 'q':0}} 
+    model_to_params = {'ArmaAdaGrad': {'p':3, 'optimizer': Adagrad, 'loss': mse}}
+    exp.initialize(mse, problem_to_params, model_to_params)
+    exp.run_all_experiments(time_steps=3000)
+    if show:
+        exp.plot_all_problem_results(time=time)
+    print("test_adagrad passed")
 
 if __name__ == "__main__":
-	test_adagrad(time=3)
+    test_adagrad(time=3)
