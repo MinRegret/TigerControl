63a834399da27a851869ec1115f7c8cbb9b2a744
AlexanderJYu <vengencemage@gmail.com>
2019-07-19 15:32:46 -0400

    optimizer stuff

--- ctsb/models/tests/test_optimizer.py
+++ ctsb/models/tests/test_optimizer.py
@@ -1,7 +1,7 @@
 import ctsb
 from ctsb.models.optimizers.losses import mse
 from ctsb.utils.experiment import Experiment
-from ctsb.models.optimizers.Adagrad import Adagrad
+from ctsb.models.optimizers.OGD import OGD
 def test_sgd_arma():
 	problem = ctsb.problem('ARMA-v0')
 	problem.initialize(p=2,q=0)
@@ -19,10 +19,21 @@ def test_adagrad_arma():
 	exp = Experiment()
 	# MSE = lambda y_true, y_pred: (y_true - y_pred)**2
 	problem_to_params = {'ARMA-v0' : {'p':2, 'q':0}} 
-	model_to_params = {'ArmaAdaGrad': {'p':3, 'optimizer': Adagrad, 'loss': mse}}
+	model_to_params = {'ArmaAdaGrad': {'p':2, 'optimizer': Adagrad, 'loss': mse}}
 	exp.initialize(mse, problem_to_params, model_to_params)
 	exp.run_all_experiments(time_steps=3000)
 	exp.plot_all_problem_results()
 
+def test_OGD_arma():
+	problem = ctsb.problem('ARMA-v0')
+	problem.initialize(p=2,q=0)
+	exp = Experiment()
+	# MSE = lambda y_true, y_pred: (y_true - y_pred)**2
+	problem_to_params = {'ARMA-v0' : {'p':2, 'q':0}} 
+	model_to_params = {'ArmaOgd': {'p':2, 'optimizer': OGD, 'loss': mse}}
+	exp.initialize(mse, problem_to_params, model_to_params)
+	exp.run_all_experiments(time_steps=1000)
+	exp.plot_all_problem_results()
+
 if __name__ == "__main__":
-	test_adagrad_arma()
+	test_OGD_arma()
