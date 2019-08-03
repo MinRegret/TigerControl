db276b88c26032d9c6ed3b9526f2118a3507d1c6
AlexanderJYu <vengencemage@gmail.com>
2019-08-03 17:52:31 -0400

    ilqr works on cartpole and stuff

--- ctsb/problems/control/tests/test_cartpole.py
+++ ctsb/problems/control/tests/test_cartpole.py
@@ -5,29 +5,45 @@ import time
 import ctsb
 import jax.numpy as np
 
+
 # cartpole test
 def test_cartpole(verbose=False):
     problem = ctsb.problem("CartPole-v0")
-    obs, r, done, _ = problem.initialize()
+    obs = problem.initialize()
+    print(obs)
 
-    t_start = time.time()
-    save_to_mem_ID = -1
+    model = ctsb.model("ILQR")
+    loss = lambda obs, a : obs[2]**2
+    dyn = problem.dynamics
+    x_0 = obs
+    T = 100
+    lamb = 0.1
+
+    u = model.ilqr(1, dyn, loss, x_0, T, 0.05, lamb)
+
+    # t_start = time.time()
+    # save_to_mem_ID = -1
 
     frame = 0
     score = 0
     restart_delay = 0
     saved = False
-    while time.time() - t_start < 3:
+    # while time.time() - t_start < 3:
+    for t in range(T):
         if verbose:
             problem.render()
-        time.sleep(1. / 60.)
-        a = 0.0 # no action
+        time.sleep(1. / 6.)
+        a = u[t] # no action
         obs, r, done, _ = problem.step(a)
+        # print(obs)
+        print(a)
 
         score += r
         frame += 1
+        '''
         if done:
             obs, r, done, _ = problem.initialize()
+        '''
             
     print("test_cartpole passed")
 
