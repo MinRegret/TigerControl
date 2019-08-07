d0daa2117151d52ea23181462cbc3220509a1a4f
paula-gradu <paula.gradu@gmail.com>
2019-08-07 13:01:10 -0400

    adding fast inverse and projection to ons

--- ctsb/models/optimizers/ons.py
+++ ctsb/models/optimizers/ons.py
@@ -6,30 +6,54 @@ from ctsb.models.optimizers.core import Optimizer
 from ctsb.models.optimizers.losses import mse
 from jax import jit, grad
 import jax.numpy as np
+import cvxopt
 
+cvxopt.solvers.options['show_progress'] = False
 
 class ONS(Optimizer):
     """
     Online newton step algorithm.
     """
 
-    def __init__(self, pred=None, loss=mse, learning_rate=1.0, hyperparameters={}):
+    def __init__(self, pred=None, loss=mse, hyperparameters={}):
         
         self.initialized = False
         
-        self.lr = learning_rate
         self.max_norm = 1.
         
-        self.hyperparameters = {'beta': 20., 'eps' : 0.1}
+        self.hyperparameters = {'beta':20., 'eps':0.1, 'fast_inverse':False, 'project':False, 'full_matrix':False}
         self.hyperparameters.update(hyperparameters)
         self.beta, self.eps = self.hyperparameters['beta'], self.hyperparameters['eps']
+        self.fast_inverse, self.project = self.hyperparameters['fast_inverse'], self.hyperparameters['project']
+        self.full_matrix = self.hyperparameters['full_matrix']
 
-        self.A = None
+        self.A, self.Ainv = None, None
 
-        self.pred = pred
-        self.loss = loss
+        self.pred, self.loss = pred, loss
+
         if self._is_valid_pred(pred, raise_error=False) and self._is_valid_loss(loss, raise_error=False):
             self.set_predict(pred, loss=loss)
+
+    def norm_project(self, y, A, c):
+        """ 
+            Project y using norm A on the convex set bounded by c.
+        """
+
+        if (np.all(np.absolute(y) <= c)): # check if y already in K
+            return y
+
+        P = cvxopt.matrix(A)
+        q = cvxopt.matrix(-A.dot(y))
+        G = cvxopt.matrix(np.append(np.identity(len(y)), -np.identity(len(y)), axis=0), tc='d')
+        h = cvxopt.matrix(np.repeat(c, 2 * len(y)), tc='d')
+
+        return (np.array([1]) * cvxopt.solvers.qp(P, q, G, h)['x']).reshape(len(y),) # run quadratic program
+
+    def general_norm(self, x):
+        x = np.asarray(x)
+        if np.ndim(x) == 0:
+            x = x[None]
+        return np.linalg.norm(x)
 
     def update(self, params, x, y, loss=mse):
         """
@@ -46,26 +70,40 @@ class ONS(Optimizer):
 
         grad = self.gradient(params, x, y, loss=loss) # defined in optimizers core class
 
+        # Make everything a list for generality
+        if(type(params) is not list):
+            params = [params]
+            grad = [grad]
+
+        grad = [np.ravel(dw) for dw in grad]
+
+        # initualize A
         if(self.A is None):
-            if(type(params) is list):
-                self.A = [np.eye(dw.shape[0]) * self.eps for dw in grad]
-            else:
-                self.A = np.eye(grad.shape[0]) * self.eps # initialize
+            self.A = [np.eye(dw.shape[0]) * self.eps for dw in grad]
+            if(self.fast_inverse):
+                self.Ainv = [np.eye(dw.shape[0]) * (1 / self.eps) for dw in grad]
 
-        if(type(params) is list):
-            for i in range(len(self.A)):
-                self.A[i] += grad[i] @ grad[i].T # update
+        for i in range(len(grad)):
+            self.A[i] += grad[i] @ grad[i].T # update
+            # PROBLEM: grad[i] shape is 40x10!!!
+            if(self.fast_inverse): 
+                self.Ainv[i] -= (self.Ainv[i] @ grad[i]) @ (self.Ainv[i] @ grad[i]).T / \
+                                (1 + grad[i].T @ self.Ainv[i] @ grad[i])
+
+        # compute max norm for normalization                       
+        self.max_norm = np.maximum(self.max_norm, np.linalg.norm([self.general_norm(dw) for dw in grad]))
+        eta = (1 / self.max_norm) * (1. / self.beta)
+
+        if(self.fast_inverse):
+            new_params = [w - eta * np.reshape(Ainv @ dw, w.shape) \
+                        for (w, Ainv, dw) in zip(params, self.Ainv, grad)]
         else:
-            self.A += grad @ grad.T # update
+            new_params = [w - eta * np.reshape(np.linalg.inv(A) @ dw, w.shape) \
+                                for (w, A, dw) in zip(params, self.A, grad)]
 
-        if (type(params) is list):
-            self.max_norm = np.maximum(self.max_norm, np.linalg.norm([np.linalg.norm(dw) for dw in grad]))
-            lr = self.lr / self.max_norm # retained for normalization
-            return [w - lr * (1. / self.beta) * np.linalg.inv(A) @ dw for (w, A, dw) in zip(params, self.A, grad)]
-
-        self.max_norm = np.maximum(self.max_norm, np.linalg.norm(grad))
-        lr = self.lr / self.max_norm # retained for normalization
-        return params - lr * (1. / self.beta) * np.linalg.inv(self.A) @ grad
-
-
+        if(self.project):
+            return [self.norm_project(p, A, 2 * self.general_norm(y)) \
+                    for (p, A) in zip(new_params, self.A)]
+        
+        return new_params
 
