218498df2fc4058d6d8d9055245c1439e867c35a
paula-gradu <paula.gradu@gmail.com>
2019-10-18 17:01:00 -0400

    gpc works faster

--- tigercontrol/methods/control/gpc.py
+++ tigercontrol/methods/control/gpc.py
@@ -32,16 +32,12 @@ class GPC(ControlMethod):
             A,B (float/numpy.ndarray): system dynamics
             n (float/numpy.ndarray): dimension of the state
             m (float/numpy.ndarray): dimension of the controls
-            H (postive int): length of history of the controller 
-            HH (positive int): length of history/memory of the system
+            H (postive int): history of the controller 
+            HH history of the system 
             x (float/numpy.ndarray): current state
             past_w (float/numpy.ndarray)  previous perturbations 
         """
         self.initialized = True
-        self.is_online = True
-        self.n, self.m = n, m # dimension of state x and control u
-        self.A, self.B = A, B # system dynamics
-        self.H, self.HH = H, HH ## length of controller history and memory for loss function
         
         def _update_past(self_past, x):
             new_past = np.roll(self_past, self.n)
@@ -49,7 +45,7 @@ class GPC(ControlMethod):
             return new_past
         self._update_past = jit(_update_past)
 
-        if (K is None):
+        if(K is None):
             # solve the ricatti equation 
             X = scipy.linalg.solve_continuous_are(A, B, np.identity(n), np.identity(m))
             #compute LQR gain
@@ -57,29 +53,40 @@ class GPC(ControlMethod):
         else:
             self.K = K
 
-        # stores previous state and action
-        self.x = np.zeros(n)
+        self.x = np.zeros(n)        
         self.u = np.zeros(m)
         
+        self.n = n   ## dimension of  the state x 
+        self.m = m   ## dimension of the control u
+        self.A = A
+        self.B = B
+        self.H = H   ## how many control matrices
+        self.HH = HH ## how many times to unfold the recursion
+
         ## internal parmeters to the class 
-        self.T = 0 ## keep track of iterations, for the learning rate
-        self.learning_rate = 1.0
-        self.M = random.normal(generate_key(), shape=(H, m, n)) / np.sqrt(0.5*(n+m)) ## CANNOT BE SET TO ZERO, Glorot init
+        self.T = 1 ## keep track of iterations, for the learning rate
+        self.learning_rate = 1
+        self.M = random.normal(generate_key(), shape=(H, m, n)) / np.sqrt(0.5*(n+m)) # Glorot CANNOT BE SET TO ZERO
         self.S = np.repeat(B.reshape(1, n, m), HH, axis=0) # previously [B for i in range(HH)]
         for i in range(1, HH):
             self.S = jax.ops.index_update(self.S, i, (A - B @ self.K) @ self.S[i-1]) 
-        self.w_past = np.zeros((HH+H,n)) ## this are the previous perturbations, from most recent [0] to latest [HH-1]
+        self.w_past = np.zeros((HH + H,n)) ## this are the previous perturbations, from most recent [0] to latest [HH-1]
 
-        # This is the counterfactual loss function, we prefer not to differentiate it and use JAX 
+        self.is_online = True
+
         def the_complicated_loss_function(M, w_past, S):
-            final = 0.0
+            """
+            This is the counterfactual loss function, we prefer not to differentiate it and use JAX 
+            """
+            final = np.zeros(self.n)
             for i in range(self.HH):
                 temp = np.tensordot(M, w_past[i:i+self.H], axes=([0,2],[0,1]))
                 final = final + S[i] @ temp
             return np.sum(final ** 2)
+
         self.grad_fn = jit(grad(the_complicated_loss_function))  # compiled gradient evaluation function
 
-    def plan(self, x_new):
+    def plan(self,x_new):
         """
         Description: Updates internal parameters and then returns the estimated optimal action (only one)
         Args:
@@ -87,14 +94,18 @@ class GPC(ControlMethod):
         Returns:
             Estimated optimal action
         """
-        self.T +=1
-        self.learning_rate = self.learning_rate / np.sqrt(self.T)
 
-        w_new = x_new - np.dot(self.A , self.x)  - np.dot(self.B , self.u) # dimension self.n
+        self.T +=1
+        self.learning_rate = 1 / np.sqrt(self.T + 1)
+
+        w_new = x_new - np.dot(self.A , self.x)  - np.dot(self.B , self.u)
         self.w_past = self._update_past(self.w_past, w_new)
         self.x = x_new
+
         self.u = - self.K @ x_new + np.tensordot(self.M, self.w_past[:self.H], axes=([0,2],[0,1]))
+        
         self.M = self.M - self.learning_rate * self.grad_fn(self.M, self.w_past, self.S)
+        
         return self.u
 
 
