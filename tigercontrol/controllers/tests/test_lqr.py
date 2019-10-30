c54b7b5fac78b21484f0d04b61eb99ca397a28b1
paula-gradu <paula.gradu@gmail.com>
2019-10-30 18:42:10 -0400

    Update test_lqr.py

--- tigercontrol/controllers/tests/test_lqr.py
+++ tigercontrol/controllers/tests/test_lqr.py
@@ -1,79 +1,61 @@
-# test the LQR controller class
+# test the LQR method class
 
 import tigercontrol
 import jax.numpy as np
-import numpy.random as random
 import matplotlib.pyplot as plt
+from tigercontrol.controllers import Controller
+import jax
+
+def get_trajectory(environment, controller, T = 100):
+
+    (environment_id, environment_params) = environment
+    (controller_id, controller_params) = controller
+    
+    environment = tigercontrol.environment(environment_id)
+    x = environment.initialize(**environment_params)
+    
+    controller_params['A'], controller_params['B'] = environment.A, environment.B
+    controller_params['x'] = x
+    
+    controller = tigercontrol.controllers(controller_id)
+    controller.initialize(**controller_params)
+    
+    trajectory = []
+    norms = []
+    avg_regret = []
+    cur_avg = 0
+    
+    u = controller.plan()
+
+    for i in range(T):
+        x = environment.step(u[i])
+        trajectory.append(x)
+        norms.append(np.linalg.norm(x))
+        cur_avg = (i / (i + 1)) * cur_avg + (np.linalg.norm(x) + np.linalg.norm(u[i])) / (i + 1)
+        avg_regret.append(cur_avg)
+            
+    return trajectory, norms, avg_regret
 
 def test_lqr(steps=10, show_plot=True):
 
-    controller = tigercontrol.controller("LQR")
-    lambda1 = 0.5
-    lambda2 = 10
+    T = 500
 
-    # Test functionality for floats
-    F = np.reshape(np.ones(2), (1, 2))
-    f = 0
-    C = np.array([[1, 0], [0, lambda1]])
-    c = 0
-    T = steps 
-    x = 1
+    n = 1 # dimension of  the state x 
+    m = 1 # control dimension
+    noise_magnitude = 0.2
+    noise_distribution = 'normal'
 
-    controller.initialize(F, f, C, c, T, x)
+    environment_id = "LDS-v0"
+    environment_params = {'n': n, 'm' : m, 'noise_magnitude' : noise_magnitude, 'noise_distribution' : noise_distribution}
 
-    u = controller.plan(x, T)
+    C = np.identity(n + m) # quadratic cost
+    LQR_params = {'C' : C, 'T' : T}
 
-    # Plot control found
-    if show_plot:
-        plt.plot([np.linalg.norm(i) for i in u], label = "LQR on floats small lambda")
-
-    C = np.array([[1, 0], [0, lambda2]])
-
-    controller.initialize(F, f, C, c, T, x)
-
-    u = controller.plan(x, T)
-
-    # Plot control found
-    if show_plot:
-        plt.plot([np.linalg.norm(i) for i in u], 'C0--', label = "LQR on floats big lambda")
-
-    # Test functionality for higher dimensions
-    lambda1 = 0.5
-    lambda2 = 100
-
-    xdim, udim = 2, 3
-
-    F = np.ones((xdim, xdim + udim))
-    f = np.zeros((xdim, 1))
-    C = np.array([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, lambda1, 0, 0], [0, 0, 0, lambda1, 0], [0, 0, 0, 0, lambda1]])
-    c = np.zeros((xdim + udim, 1))
-    T = steps
-    x = np.ones((xdim, 1))
-
-    controller.initialize(F, f, C, c, T, x)
-
-    u = controller.plan(x, T)
-
-    # Plot control found
-    if show_plot:
-        plt.plot([np.linalg.norm(i) for i in u], label = "LQR on multiple dim small lambda")
-
-    C = np.array([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, lambda2, 0, 0], [0, 0, 0, lambda2, 0], [0, 0, 0, 0, lambda2]])
-
-    controller.initialize(F, f, C, c, T, x)
-
-    u = controller.plan(x, T)
-
-    # Plot control found
-    if show_plot:
-        plt.plot([np.linalg.norm(i) for i in u], 'C1--', label = "LQR on multiple dim big lambda")
-
-    if show_plot:
-        plt.title("LQR")
-        plt.legend()
-        plt.show(block=False)
-        plt.pause(3)
-        plt.close()
+    LQR_results, LQR_norms, LQR_avg_results = get_trajectory((environment_id, environment_params), \
+                                                            ('LQR', LQR_params), T = T)
+    if(show_plot):
+        plt.plot(LQR_norms, label = "LQR")
+        plt.title("LQR on LDS");
 
     print("test_lqr passed")
     return
