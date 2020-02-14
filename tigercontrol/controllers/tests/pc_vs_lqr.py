3ab7597cdc3ebde2b9ef623622145875a9e7435a
paula-gradu <paula.gradu@gmail.com>
2020-02-14 16:34:16 -0500

    Add files via upload

---
+++ tigercontrol/controllers/tests/pc_vs_lqr.py
@@ -0,0 +1,95 @@
+# test the LQR infinite horizon method class
+
+import tigercontrol
+import jax.numpy as np
+import matplotlib.pyplot as plt
+import jax
+import pandas as pd
+import numpy as onp
+import numpy.random as random
+import seaborn as sns
+from tigercontrol.controllers.lqr import LQR
+from tigercontrol.controllers.bpc import BPC
+from tigercontrol.controllers.gpc_v1 import GPC_v1
+from tigercontrol.controllers.gpc_v2 import GPC_v2
+
+T = 500
+alg_name = ['LQR', 'GPC-v1', 'GPC-v2', 'BPC']
+color_code = {'LQR': 'blue', 'GPC-v1': 'red', 'GPC-v2': 'orange', 'BPC': 'purple'}
+quad = lambda x, u: np.sum(x.T @ x + u.T @ u)
+
+def evaluate(controller, Wgen, cost_fn):
+    global T
+    x, loss = np.zeros((n, 1)), [0. for _ in range(T)]
+    for t in range(T):
+        u = controller.get_action(x)
+        loss[t] = cost_fn(x, u)
+        controller.update(loss[t])
+        x = A @ x + B @ u + Wgen.next()
+
+    return np.array(loss, dtype=np.float32), \
+        np.array(np.cumsum(loss)/np.arange(T), dtype=np.float32)
+
+
+def to_dataframe(alg, loss):
+    inst_loss, avg_loss = loss
+    return pd.DataFrame(data = {'Algorithm': alg, 'Time': np.arange(T, dtype=np.float32),
+                                'Instantaneous Cost': loss, 'Average Cost': avg_loss})
+
+def benchmark(Wgen, cost_fn = quad):
+
+    loss_bpc = evaluate(LQR(A, B), Wgen, cost_fn)
+    loss_gpc_1 = evaluate(GPC_v1(A, B, cost_fn), Wgen, cost_fn)
+    loss_gpc_2 = evaluate(GPC_v2(A, B, cost_fn), Wgen, cost_fn)
+    loss_bpc = evaluate(BPC(A, B), Wgen, cost_fn)
+
+    return loss_lqr, loss_gpc_1, loss_gpc_2, loss_bpc
+
+def repeat_benchmark(M, Wgen, rep = rep, cost_fn = quad):
+    all_data = []
+    for r in range(rep):
+        loss = benchmark(Wgen, cost_fn)
+        data = pd.concat(list(map(lambda x: to_dataframe(*x), list(zip(alg_name, loss)))))
+        all_data.append(data)
+    all_data = pd.concat(all_data)
+    return all_data[all_data['Instantaneous Cost'].notnull()]
+
+def plot(title, data, scale = 'linear'):
+    fig, axs = plt.subplots(ncols=2, figsize=(15,4))
+    axs[0].set_yscale(scale)
+    sns.lineplot(x = 'Time', y = 'Instantaneous Cost', hue = 'Algorithm', 
+                 data = data, ax = axs[0], ci = 'sd', palette = color_code).set_title(title)
+    axs[1].set_yscale(scale)
+    sns.lineplot(x = 'Time', y = 'Average Cost', hue = 'Algorithm', 
+                 data = data, ax = axs[1], ci = 'sd', palette = color_code).set_title(title)
+
+class Wgen:
+    def __init__(self, n, m):
+        global T
+        self.t = 0
+        self.w = (np.sin(np.arange(T*m)/(32*np.pi)).reshape(T,m) @ np.ones((m, n))).reshape(T, n, 1)
+
+    def next(self):
+        self.t += 1
+        return self.w[self.t]
+
+def test_pc_vs_lqr(T=500, rep = 3, show_plot=True):
+
+    T = T
+
+    n, m, A, B = 2, 1, np.array([[1., 1.], [0., 1.]]), np.array([[0.], [1.]])
+    Q, R = np.eye(N = n), np.eye(N = m)
+
+    A = 0.9 * A / np.linalg.norm(A, ord='nuc')
+    B = 0.9 * B / np.linalg.norm(B, ord='nuc')
+
+    quad_cost = lambda x, u: (x.T @ Q @ x + u.T @ R @ u)[0][0]
+
+    data = repeat_benchmark(Wgen(T, n, m), rep = 1)
+    plot('Sinusoidal Perturbations', data)
+
+    print("test_gpc passed")
+    return
+
+if __name__=="__main__":
+    test_gpc()
