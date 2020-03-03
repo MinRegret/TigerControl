14b017d6fb02e00bd1499eefea250ae77d311ca9
paula-gradu <paula.gradu@gmail.com>
2020-03-03 16:37:45 -0500

    Delete __init__.py

--- tigercontrol/experiments/__init__.py
+++
@@ -1,7 +0,0 @@
-# experiments init file
-
-from tigercontrol.experiments.metrics import *
-from tigercontrol.experiments.core import create_full_environment_to_controllers, run_experiment
-from tigercontrol.experiments.new_experiment import NewExperiment
-from tigercontrol.experiments.experiment import Experiment
-from tigercontrol.experiments.precomputed import recompute, load_prob_controller_to_result
