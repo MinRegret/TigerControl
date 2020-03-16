34f3c5851056a3bd6be42044043e027248c40b58
paula-gradu <paula.gradu@gmail.com>
2020-03-15 21:10:38 -0400

    Update core.py

--- tigercontrol/controllers/core.py
+++ tigercontrol/controllers/core.py
@@ -1,21 +1,21 @@
-# Controller class
-# Author: John Hallman
+# Controller class 
 
+import tigercontrol
+import jax
+import jax.numpy as np
+from tigercontrol.controllers import Controller
 from tigercontrol import error
-from tigercontrol.utils.optimizers import Optimizer
 
 # class for implementing algorithms with enforced modularity
-class Controller(object):
+# ---------- Losses ----------
 
-    def plan(self, x, horizon, **kwargs):
-        # returns a series of actions (a plan), given current observation x
-        raise NotImplementedError
+class Controller(object):
 
     def get_action(self, x, replan=False, horizon=None):
         """ Description: returns action based on input state x """
         raise NotImplementedError
 
-    def update(self, **kwargs):
+    def update_parameters(self, **kwargs):
         # update parameters according to given loss and update rule
         raise NotImplementedError
 
@@ -26,15 +26,48 @@ class Controller(object):
         return self.__str__()
         
 
-""" # OLD CODE
-    def get_action(self, x, replan=False, horizon=None):
-        if horizon == None: horizon = self.T
-        if hasattr(self, "plan_cache") and not replan:
-            u = self.plan_cache.pop(0)
-            if len(self.plan_cache) == 0:
-                self.plan_cache = self.plan(x, horizon)
-        else:
-            self.plan_cache = self.plan(x, horizon)
-            u = self.plan_cache.pop(0)
-        return u
-"""
+# ---------- Losses ----------
+
+# Quadratic Loss
+def quad_loss(x, u, Q = None, R = None):
+	x_contrib = x.T @ x if Q is None else x.T @ Q @ x
+	u_contrib = u.T @ u if R is None else u.T @ R @ u
+	
+	return np.sum(x_contrib + u_contrib)
+
+# Policy Loss
+def policy_loss(params, determine_action, w, look_back, env, cost_fn = quad_loss):
+    """
+    Description: 
+    Args:
+    """
+    y = np.zeros((env.n, 1))
+    for h in range(look_back, 0, -1):
+        v = determine_action(params, y, w[:-h])
+        y = env.dyn(y, v) + w[-h] 
+
+    # Don't update state at the end    
+    v = determine_action(params, y, w)
+    return cost_fn(y, v) 
+
+# Action Loss
+def action_loss(actions, w, look_back, env, cost_fn = quad_loss):
+    """
+    Description: 
+    Args:
+    """
+    y = np.zeros((env.n, 1))
+    for h in range(look_back, 1, -1):
+        y = env.dyn(y, actions[-h]) + w[-h-1]
+
+    # Don't update state at the end    
+    v = actions[-1]
+    return cost_fn(y, v) 
+
+
+# ---------- History Updates ----------
+
+def update_noise(w, x, u, env):
+    w = jax.ops.index_update(w, 0, x - env.dyn(x, u))
+    w = np.roll(w, -1, axis = 0) # new noise will be located at w[-1]
+    return w
