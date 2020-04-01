f877f59a9c3c6f19495c4c3ec422ef0758e6283b
John Hallman <johnolof@gmail.com>
2020-03-31 17:35:32 -0700

    quadcopter now uses jax numpy

--- deprecated/environments/test_quadcopter.py
+++ deprecated/environments/test_quadcopter.py
@@ -2,26 +2,24 @@
 # Author: John Hallman
 
 import tigercontrol
-#from tigercontrol.deprecated.controllers.quadcopter_controller import QuadcopterModel
-
-# Perceptron for the quadcopter environment
 from tigercontrol.controllers import Controller
-import numpy as np
+import jax
+import jax.numpy as np
 
 
 # run test
 def test_quadcopter():
     # Controller parameters
     CONTROLLER_PARAMETERS = {
-        'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
-        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+        'Linear_PID':{'P':np.array([300,300,7000]),'I':np.array([0.04,0.04,4.5]),'D':np.array([450,450,5000])},
+        'Angular_PID':{'P':np.array([22000,22000,1500]),'I':np.array([0,0,1.2]),'D':np.array([12000,12000,0])},
     }
-    GOAL = (0,0,2)
+    GOAL = np.array([0, 0, 2]) # x, y, z
     YAW = 0
 
     # initialize quadcopter and GUI
-    quad_environment_class = tigercontrol.environment("Quadcopter")
-    quad_environment = quad_environment_class()
+    quad_environment = tigercontrol.environment("Quadcopter")
+    #quad_environment = quad_environment_class()
     state = quad_environment.reset()
 
     # initialize model and targets
@@ -58,60 +56,57 @@ class QuadcopterModel(Controller):
         self.ANGULAR_I = params['Angular_PID']['I']
         self.ANGULAR_D = params['Angular_PID']['D']
 
-        self.xi_term = 0
-        self.yi_term = 0
-        self.zi_term = 0
+        self.p_i_term = np.zeros(3)
+        self.a_i_term = np.zeros(3)
         self.thetai_term = 0
         self.phii_term = 0
         self.gammai_term = 0
         self.thread_object = None
-        self.target = [0,0,0]
-        self.yaw_target = 0.0
-        self.run = True
-        self.initialized = True
         self.target = goal
         self.yaw_target = self._wrap_angle(yaw)
-        
+
+        @jax.jit
+        def _get_action(obs, p_i_term, a_i_term):
+            p, p_dot, a, a_dot = obs[:3], obs[3:6], obs[6:9], obs[9:12]
+            theta, phi, gamma = a
+            theta_dot, phi_dot, gamma_dot = a_dot
+
+            err = self.target - p
+            p_i_term += self.LINEAR_I * err
+            dest_dot = self.LINEAR_P * err - self.LINEAR_D * p_dot + p_i_term
+            dest_x_dot, dest_y_dot, dest_z_dot = dest_dot
+            throttle = np.clip(dest_z_dot, self.Z_LIMITS[0], self.Z_LIMITS[1])
+
+            dest_theta = self.LINEAR_TO_ANGULAR_SCALER[0]*(dest_x_dot*np.sin(gamma)-dest_y_dot*np.cos(gamma))
+            dest_phi = self.LINEAR_TO_ANGULAR_SCALER[1]*(dest_x_dot*np.cos(gamma)+dest_y_dot*np.sin(gamma))
+            dest_gamma = self.yaw_target
+            dest_theta = np.clip(dest_theta,self.TILT_LIMITS[0],self.TILT_LIMITS[1])
+            dest_phi = np.clip(dest_phi,self.TILT_LIMITS[0],self.TILT_LIMITS[1])
+            theta_error = dest_theta-theta
+            phi_error = dest_phi-phi
+            gamma_dot_error = (self.YAW_RATE_SCALER*self._wrap_angle(dest_gamma-gamma)) - gamma_dot
+
+            a_err = np.array([theta_error, phi_error, gamma_dot_error])
+            a_i_term += self.ANGULAR_I * a_err
+            p_val = self.ANGULAR_P * a_err - self.ANGULAR_D * a_dot + a_i_term
+            x_val, y_val, z_val = p_val
+            z_val = np.clip(z_val, self.YAW_CONTROL_LIMITS[0],self.YAW_CONTROL_LIMITS[1])
+
+            m1 = throttle + x_val + z_val
+            m2 = throttle + y_val - z_val
+            m3 = throttle - x_val + z_val
+            m4 = throttle - y_val - z_val
+            M = np.array([m1,m2,m3,m4])
+            M = np.clip(M, self.MOTOR_LIMITS[0], self.MOTOR_LIMITS[1])
+            return M, p_i_term, a_i_term
+        self._get_action = _get_action
+
     def _wrap_angle(self,val):
         return (val + np.pi) % (2 * np.pi) - np.pi
 
     def get_action(self, obs): # weights can be fount at the end of the file
-        [dest_x,dest_y,dest_z] = self.target
-        [x,y,z,x_dot,y_dot,z_dot,theta,phi,gamma,theta_dot,phi_dot,gamma_dot] = obs
-        x_error = dest_x-x
-        y_error = dest_y-y
-        z_error = dest_z-z
-        self.xi_term += self.LINEAR_I[0]*x_error
-        self.yi_term += self.LINEAR_I[1]*y_error
-        self.zi_term += self.LINEAR_I[2]*z_error
-        dest_x_dot = self.LINEAR_P[0]*(x_error) + self.LINEAR_D[0]*(-x_dot) + self.xi_term
-        dest_y_dot = self.LINEAR_P[1]*(y_error) + self.LINEAR_D[1]*(-y_dot) + self.yi_term
-        dest_z_dot = self.LINEAR_P[2]*(z_error) + self.LINEAR_D[2]*(-z_dot) + self.zi_term
-        throttle = np.clip(dest_z_dot,self.Z_LIMITS[0],self.Z_LIMITS[1])
-        dest_theta = self.LINEAR_TO_ANGULAR_SCALER[0]*(dest_x_dot*np.sin(gamma)-dest_y_dot*np.cos(gamma))
-        dest_phi = self.LINEAR_TO_ANGULAR_SCALER[1]*(dest_x_dot*np.cos(gamma)+dest_y_dot*np.sin(gamma))
-        dest_gamma = self.yaw_target
-        dest_theta,dest_phi = np.clip(dest_theta,self.TILT_LIMITS[0],self.TILT_LIMITS[1]),np.clip(dest_phi,self.TILT_LIMITS[0],self.TILT_LIMITS[1])
-        theta_error = dest_theta-theta
-        phi_error = dest_phi-phi
-        gamma_dot_error = (self.YAW_RATE_SCALER*self._wrap_angle(dest_gamma-gamma)) - gamma_dot
-        self.thetai_term += self.ANGULAR_I[0]*theta_error
-        self.phii_term += self.ANGULAR_I[1]*phi_error
-        self.gammai_term += self.ANGULAR_I[2]*gamma_dot_error
-        x_val = self.ANGULAR_P[0]*(theta_error) + self.ANGULAR_D[0]*(-theta_dot) + self.thetai_term
-        y_val = self.ANGULAR_P[1]*(phi_error) + self.ANGULAR_D[1]*(-phi_dot) + self.phii_term
-        z_val = self.ANGULAR_P[2]*(gamma_dot_error) + self.gammai_term
-        z_val = np.clip(z_val,self.YAW_CONTROL_LIMITS[0],self.YAW_CONTROL_LIMITS[1])
-        m1 = throttle + x_val + z_val
-        m2 = throttle + y_val - z_val
-        m3 = throttle - x_val + z_val
-        m4 = throttle - y_val - z_val
-        M = np.clip([m1,m2,m3,m4],self.MOTOR_LIMITS[0],self.MOTOR_LIMITS[1])
-        return M
-
-
-
-
+        a, self.p_i_term, self.a_i_term = self._get_action(obs, self.p_i_term, self.a_i_term)
+        return a
 
 if __name__ == "__main__":
     test_quadcopter()
--- tigercontrol/environments/quadcopter.py
+++ tigercontrol/environments/quadcopter.py
@@ -1,27 +1,17 @@
 """ Quadcopter environment implementation """
 
-#import jax.numpy as np
-import numpy as np
-import scipy.integrate
+import numpy as onp
+import jax.numpy as np
+import jax
+import jax.random as random
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as Axes3D
+
 from tigercontrol.environments import Environment
+from tigercontrol.utils import generate_key
 
 
 class Quadcopter(Environment):
-    class Propeller(): # propeller helper class 
-        def __init__(self, prop_dia, prop_pitch):
-            self.dia = prop_dia
-            self.pitch = prop_pitch
-            self.speed = 0 #RPM
-            self.thrust = 0
-
-        def set_speed(self, speed):
-            self.speed = speed
-            # From http://www.electricrcaircraftguy.com/2013/09/propeller-static-dynamic-thrust-equation.html
-            self.thrust = 4.392e-8 * self.speed * np.power(self.dia,3.5)/(np.sqrt(self.pitch))
-            self.thrust = self.thrust*(4.23e-4 * self.speed * self.pitch)
-
     # State space representation: [x y z x_dot y_dot z_dot theta phi gamma theta_dot phi_dot gamma_dot]
     # From Quadcopter Dynamics, Simulation, and Control by Andrew Gibiansky
     def __init__(self):
@@ -32,44 +22,67 @@ class Quadcopter(Environment):
         self.L = 0.3 # quadcopter width/length
         self.prop_dia = 10.0 # propeller diameter
         self.prop_pitch = 4.5 # propeller pitch
-        self.map_width = 2.0
-        self.map_height = 6.0
-        self.ode = scipy.integrate.ode(self._state_dot).set_integrator('vode', nsteps=5, method='bdf')
-        self.prop1 = self.Propeller(self.prop_dia, self.prop_pitch)
-        self.prop2 = self.Propeller(self.prop_dia, self.prop_pitch)
-        self.prop3 = self.Propeller(self.prop_dia, self.prop_pitch)
-        self.prop4 = self.Propeller(self.prop_dia, self.prop_pitch)
+        self.map_width = 4.0
+        self.map_height = 8.0
+        self.dt = 0.01 # seconds
         # From Quadrotor Dynamics and Control by Randal Beard
         ixx=((2*self.weight*self.yaw_rate**2)/5)+(2*self.weight*self.L**2)
         iyy=ixx
         izz=((2*self.weight*self.yaw_rate**2)/5)+(4*self.weight*self.L**2)
         self.I = np.array([[ixx,0,0],[0,iyy,0],[0,0,izz]])
         self.invI = np.linalg.inv(self.I)
-        init_pos = np.concatenate((np.random.uniform(-self.map_width, self.map_width, size=(2,)), np.random.uniform(0,self.map_height, size=(1,))))
-        self.state = np.concatenate((init_pos, np.zeros(9))) # initial state
         self.render_init = False
 
+        @jax.jit
+        def _dynamics(s, a):
+            thrusts = a * 4.392e-8 * np.power(self.prop_dia, 3.5)/(np.sqrt(self.prop_pitch))
+            thrusts = thrusts * (4.23e-4 * a * self.prop_pitch)
+            s_new = s + self.dt * self._state_dot(s, thrusts)
+            s_new = jax.ops.index_update(s_new, jax.ops.index[6:9], self._wrap_angle(s_new[6:9]))
+            s_new = jax.ops.index_update(s_new, 2, np.max((0.0, s_new[2]))) # z >= 0 
+            return s_new
+        self._dynamics = _dynamics
+
     def reset(self):
-        self.QUAD_DYNAMICS_UPDATE = 0.010 # seconds
-        self.render()
+        # random initial state
+        xy = random.uniform(generate_key(), minval=-self.map_width, maxval=self.map_width, shape=(2,))
+        z = random.uniform(generate_key(), minval=1.0, maxval=self.map_height, shape=(1,))
+        init_pos = np.concatenate((xy, z))
+        init_pos = np.array([-3, -3, 7]) # constant starting position for debugging
+        self.state = np.concatenate((init_pos, np.zeros(9)))
+        self._render()
         return self.state
 
     def step(self, motor_action):
-        self.prop1.set_speed(motor_action[0])
-        self.prop2.set_speed(motor_action[1])
-        self.prop3.set_speed(motor_action[2])
-        self.prop4.set_speed(motor_action[3])
-        self.update(self.QUAD_DYNAMICS_UPDATE)
-        self.render()
+        self.state = self._dynamics(self.state, motor_action)
+        self._render()
         return self.state
 
-    def update(self, dt):
-        self.ode.set_initial_value(self.state,0).set_f_params('q1')
-        self.state = self.ode.integrate(dt)
-        self.state[6:9] = self._wrap_angle(self.state[6:9]) # angles
-        self.state[2] = max(0,self.state[2]) # cannot go below floor level
+    def _rotation_matrix(self, angles):
+        ct, cp, cg = np.cos(angles)
+        st, sp, sg = np.sin(angles)
+        R_x = np.array([[1,0,0],[0,ct,-st],[0,st,ct]])
+        R_y = np.array([[cp,0,sp],[0,1,0],[-sp,0,cp]])
+        R_z = np.array([[cg,-sg,0],[sg,cg,0],[0,0,1]])
+        R = np.dot(R_z, np.dot( R_y, R_x ))
+        return R
 
-    def render(self):
+    def _wrap_angle(self,val):
+        return (val + np.pi) % (2 * np.pi) - np.pi 
+
+    def _state_dot(self, s, thrusts):
+        t1, t2, t3, t4 = thrusts
+        x_dot = s[3:6] # The velocities(t+1 x_dots equal the t x_dots)
+        R = self._rotation_matrix(s[6:9]) # The acceleration
+        x_dotdot = np.array([0,0,-self.weight*self.g])+np.dot(R, np.array([0, 0, np.sum(thrusts)]))/self.weight
+        a_dot = s[9:12] # The angular rates(t+1 theta_dots equal the t theta_dots)
+        # The angular accelerations
+        tau = np.array([self.L*(t1-t3), self.L*(t2-t4), self.b*(t1-t2+t3-t4)])
+        a_dotdot = np.dot(self.invI, (tau - np.cross(a_dot, np.dot(self.I, a_dot))))
+        state_dot = np.concatenate((x_dot, x_dotdot, a_dot, a_dotdot))
+        return state_dot
+
+    def _render(self):
         if not self.render_init:
             self._init_render()
             self.render_init = True
@@ -77,9 +90,11 @@ class Quadcopter(Environment):
         L = self.L
         points = np.array([ [-L,0,0], [L,0,0], [0,-L,0], [0,L,0], [0,0,0], [0,0,0] ]).T
         points = np.dot(R,points)
-        points[0,:] += self.state[0]
-        points[1,:] += self.state[1]
-        points[2,:] += self.state[2]
+        points = jax.ops.index_add(points, jax.ops.index[0,:], self.state[0])
+        points = jax.ops.index_add(points, jax.ops.index[1,:], self.state[1])
+        points = jax.ops.index_add(points, jax.ops.index[2,:], self.state[2])
+        points = onp.array(points) # jax numpy works poorly with matplotlib 
+        
         self.l1.set_data(points[0,0:2],points[1,0:2])
         self.l1.set_3d_properties(points[2,0:2])
         self.l2.set_data(points[0,2:4],points[1,2:4])
@@ -91,58 +106,16 @@ class Quadcopter(Environment):
     def _init_render(self):
         self.fig = plt.figure()
         self.ax = Axes3D.Axes3D(self.fig)
-        self.ax.set_xlim3d([-2.0, 2.0])
+        self.ax.set_xlim3d([-self.map_width, self.map_width])
         self.ax.set_xlabel('X')
-        self.ax.set_ylim3d([-2.0, 2.0])
+        self.ax.set_ylim3d([-self.map_width, self.map_width])
         self.ax.set_ylabel('Y')
-        self.ax.set_zlim3d([0, 5.0])
+        self.ax.set_zlim3d([0, self.map_height])
         self.ax.set_zlabel('Z')
         self.ax.set_title('Quadcopter Simulation')
         self.l1, = self.ax.plot([],[],[],color='blue',linewidth=3,antialiased=False)
         self.l2, = self.ax.plot([],[],[],color='red',linewidth=3,antialiased=False)
         self.hub, = self.ax.plot([],[],[],marker='o',color='green', markersize=6,antialiased=False)
-
-    def _rotation_matrix(self, angles):
-        ct = np.cos(angles[0])
-        cp = np.cos(angles[1])
-        cg = np.cos(angles[2])
-        st = np.sin(angles[0])
-        sp = np.sin(angles[1])
-        sg = np.sin(angles[2])
-        R_x = np.array([[1,0,0],[0,ct,-st],[0,st,ct]])
-        R_y = np.array([[cp,0,sp],[0,1,0],[-sp,0,cp]])
-        R_z = np.array([[cg,-sg,0],[sg,cg,0],[0,0,1]])
-        R = np.dot(R_z, np.dot( R_y, R_x ))
-        return R
-
-    def _wrap_angle(self,val):
-        return( ( val + np.pi) % (2 * np.pi ) - np.pi )
-
-    def _state_dot(self, time, state):
-        state_dot = np.zeros(12)
-        # The velocities(t+1 x_dots equal the t x_dots)
-        state_dot[0] = self.state[3]
-        state_dot[1] = self.state[4]
-        state_dot[2] = self.state[5]
-        # The acceleration
-        x_dotdot = np.array([0,0,-self.weight*self.g]) + np.dot(self._rotation_matrix(self.state[6:9]), \
-            np.array([0,0,(self.prop1.thrust + self.prop2.thrust + self.prop3.thrust + self.prop4.thrust)])) /self.weight
-        state_dot[3] = x_dotdot[0]
-        state_dot[4] = x_dotdot[1]
-        state_dot[5] = x_dotdot[2]
-        # The angular rates(t+1 theta_dots equal the t theta_dots)
-        state_dot[6] = self.state[9]
-        state_dot[7] = self.state[10]
-        state_dot[8] = self.state[11]
-        # The angular accelerations
-        omega = self.state[9:12]
-        tau = np.array([self.L*(self.prop1.thrust-self.prop3.thrust), self.L*(self.prop2.thrust-self.prop4.thrust), \
-            self.b*(self.prop1.thrust-self.prop2.thrust+self.prop3.thrust-self.prop4.thrust)])
-        omega_dot = np.dot(self.invI, (tau - np.cross(omega, np.dot(self.I,omega))))
-        state_dot[9] = omega_dot[0]
-        state_dot[10] = omega_dot[1]
-        state_dot[11] = omega_dot[2]
-        return state_dot
 
 
 
