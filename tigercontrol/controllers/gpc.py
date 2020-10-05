57884799f29839790acdb097a5c5e3084fdce699
paula-gradu <paula.gradu@gmail.com>
2020-10-04 23:41:47 -0400

    Update gpc.py

--- tigercontrol/controllers/gpc.py
+++ tigercontrol/controllers/gpc.py
@@ -20,13 +20,13 @@ quad = lambda x, u: np.sum(x.T @ x + u.T @ u)
 # GPC definition
 class GPC(Controller):
     def __init__(self, A, B, Q = None, R = None, cost_fn = quad, \
-        H = 3, HH = 3, lr = 0.001, include_bias = True):
+        H = 3, HH = 3, lr = 0.0001):
         """
         Description: Initialize the dynamics of the model
         Args:
             A,B (float/numpy.ndarray): system dynamics
-            H (postive int): history of the controller 
-            HH (positive int): history of the system 
+            H (postive int): history of the controller
+            HH (positive int): history of the system
             K (float/numpy.ndarray): Starting policy (optional). Defaults to LQR gain.
             x (float/numpy.ndarray): initial state (optional)
         """
@@ -37,13 +37,11 @@ class GPC(Controller):
         self.A, self.B = A, B # System Dynamics
 
         self.t = 1 # Time Counter (for decaying learning rate)
-        #### PERSONAL NOTE: introduce lr schedule??
         self.lr, self.H= lr, H # Model Hyperparameters
-        self.include_bias = include_bias
 
-        # Model Parameters 
+        # Model Parameters
         # initial linear policy / perturbation contributions / bias
-        self.K, self.M, self.bias = LQR(A, B, Q, R).K, np.zeros((H, m, n)), np.zeros((m, 1))
+        self.K, self.M = LQR(A, B, Q, R).K, np.zeros((H, m, n))
 
         # Past H + HH noises
         self.w = np.zeros((H + HH, n, 1))
@@ -52,31 +50,31 @@ class GPC(Controller):
         self.x, self.u = np.zeros((n, 1)), np.zeros((m, 1))
 
         # The Surrogate Cost Function
-        def policy_loss(M, bias, w, cost_t = cost_fn):
+        def policy_loss(M, w, cost_t = cost_fn):
             y = np.zeros((n, 1))
             for h in range(HH - 1):
-                v = -self.K @ y + np.tensordot(M, w[h : h + H], axes = ([0, 2], [0, 1])) + bias
+                v = -self.K @ y + np.tensordot(M, w[h : h + H], axes = ([0, 2], [0, 1]))
                 y = A @ y + B @ v + w[h + H]
-            # Don't update state at the end    
-            v = -self.K @ y + np.tensordot(M, w[h : h + H], axes = ([0, 2], [0, 1])) + bias
-            return cost_t(y, v) 
+            # Don't update state at the end
+            h += 1
+            v = -self.K @ y + np.tensordot(M, w[h : h + H], axes = ([0, 2], [0, 1]))
+            return cost_t(y, v)
 
-        self.grad = grad(policy_loss, (0, 1))
+        self.grad = grad(policy_loss)
 
         # If cost function stays the same throughout run, jit the gradient for efficiency
-        if(cost_fn is not None):  
+        if(cost_fn is not None):
             self.grad = jit(self.grad)
 
     def update(self, grad = None, cost = None):
         # 1. Get gradients if not provided
         if(grad == None):
-            delta_M, delta_off = self.grad(self.M, self.bias, self.w, cost)
+            delta_M = self.grad(self.M, self.bias, self.w, cost)
         else:
-            delta_M, delta_off = grad, np.zeros(self.w[0].shape)
+            delta_M = grad
 
         # 2. Execute updates
         self.M -= self.lr * delta_M
-        self.bias -= self.lr * delta_off
 
     def get_action(self, x):
         # 1. Get new noise (will be located at w[-1])
@@ -90,7 +88,6 @@ class GPC(Controller):
         self.t = self.t + 1
 
         # 3. Compute and return new action
-        self.u = -self.K @ x + np.tensordot(self.M, self.w[-self.H:], \
-            axes = ([0, 2], [0, 1])) + self.bias * self.include_bias
+        self.u = -self.K @ x + np.tensordot(self.M, self.w[-self.H:], axes = ([0, 2], [0, 1]))
 
         return self.u
